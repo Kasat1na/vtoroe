a601dc28da07a0e8ccd00288786e1a80d7b6bc01
Anastasia <kasatkina05@icloud.com>
2024-05-09 20:18:57 +0300

    1

---
+++ source/repos/vtoroe/vtoroe/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace vtoroe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 9;
+            var array = new int[n, n];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                array[i, i] = i + 1;
+                array[array.GetLength(0) - i - 1, i] = i + 1;
+            }
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write($"{array[i, j]}  ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
